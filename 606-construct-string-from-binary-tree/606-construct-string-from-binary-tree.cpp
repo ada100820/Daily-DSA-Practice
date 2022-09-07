8ca8a7340dfb37b282ebcd954ad0e69cd54b2f8b
Adarsh Shukla <89727799+ada100820@users.noreply.github.com>
2022-09-07 23:57:08 +0530

    Time: 25 ms (79.57%), Space: 23.4 MB (87.31%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -11,19 +11,31 @@
  */
 class Solution {
 public:
-    string tree2str(TreeNode* root) {
-		string ans = to_string(root->val);
-		if (root->left) //left side check
-			ans += "(" + tree2str(root->left) + ")";
-		if (root->right) { //right side check
-			if (!root->left) ans += "()"; //left side not present, but right side present
-			ans += "(" + tree2str(root->right) + ")"; 
-		}
-		return ans;
-	}
+    static string tree2str(const TreeNode* root) {
+        string ans;
+        tree2Str(root, ans);
+        return ans;
+    }
+    static void tree2Str(const TreeNode* node, string& ans) {
+        if (!node) return;   
+        ans += to_string(node->val);
+        if (node->left) { //left side check
+            ans += '(';
+            tree2Str(node->left, ans);
+            ans += ')';
+        }
+        if (node->right) { //right side check
+            if (!node->left) ans += "()"; //left side not present, but right side present
+            ans += '(';
+            tree2Str(node->right, ans);
+            ans += ')';
+        }
+    }
 };
 
+
 /*
+                                              Optimize solution 
 Here we recursively traverse the nodes in binary tree and check:
 
 --> If there exists a left or right child, we add parentheses for left child and put inside whatever we find on left 
@@ -33,6 +45,6 @@ Here we recursively traverse the nodes in binary tree and check:
 --> The difference that can observed is that for left part we create parentheses even when there is no left child, but
     for right part we create parentheses only when there is right child.
 
---> Time Complexity: O(N^2), where N is the number of nodes in the Binary Tree.
+--> Time Complexity: O(N), where N is the number of nodes in the Binary Tree.
     Space Complexity: O(H), where H is the height of the Binary Tree (Recursion stack space)
 */
