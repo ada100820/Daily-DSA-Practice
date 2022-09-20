970b4fd4d560b7c34318ce0e82110b7f39c402ab
Adarsh Shukla <89727799+ada100820@users.noreply.github.com>
2022-09-20 23:53:53 +0530

    Time: 519 ms (46.59%), Space: 11.7 MB (99.23%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+        vector<int> dp(m, 0);
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+            for (int j = m-1; j >= 0; j--) {
                 if (nums1[i] == nums2[j]) {
-                    if (i == 0 || j == 0) dp[i][j] = 1;
-                    else dp[i][j] = dp[i-1][j-1] + 1;
-                    ans = max(ans, dp[i][j]);
+                    if (i == 0 || j == 0) dp[j] = 1;
+                    else dp[j] = dp[j-1] + 1;
+                    ans = max(ans, dp[j]);
                 }
+                else dp[j] = 0;
             }
         }
         return ans;
@@ -19,16 +20,19 @@ public:
 
 
 
-/* By (DP Tabulation):
+
+/* 
+(DP Space Optimization):
 
 Intuition:
-We can use a 2D DP array to store the length of the longest common subarray ending at nums1[i] and nums2[j].
-The recurrence relation is dp[i][j] = dp[i-1][j-1] + 1 if nums1[i] == nums2[j], otherwise dp[i][j] = 0.
+We can use a 1D DP array to store the length of the longest common subarray ending at nums1[i] and nums2[j].
+The recurrence relation is dp[j] = dp[j-1] + 1 if nums1[i] == nums2[j], otherwise dp[j] = 0.
 
 Approach:
---> Create a 2D DP array of size (nums1.size() + 1) * (nums2.size() + 1) and initialize it with 0.
+--> Create a 1D DP array of size (nums2.size() + 1) and initialize it with 0.
 --> Iterate over the DP array and update the values using the recurrence relation.
 --> Return the maximum value in the DP array.
 --> Time Complexity: O(n*m), where n is the size of nums1 and m is the size of nums2.
---> Space Complexity: O(n*m)
+--> Space Complexity: O(m)
+
 */
