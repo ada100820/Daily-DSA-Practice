3bc1d1530e7df5be9b2bc1c465134b2a2bbf9c09
Adarsh Shukla <89727799+ada100820@users.noreply.github.com>
2022-09-25 23:24:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (24.59%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -3,16 +3,17 @@
 class Solution {
     public:
     int smallestEvenMultiple(int n) {
-        if(n==0)
+        if(n==0)                                       //if the given number will be equal to zero
         {
             return 0;
         }
-        int count=1;
+        int count=1;                                   // count variable for the checking on each index 
         while(true){
-            if(count%2==0 && count%n==0){
-                return count;
+            if(count%2==0 && count%n==0)               // if count is divisible by both 2 and n
+            {
+                return count;                          // return that index of the count
             }
-            count++;
+            count++;                                         
         }
         return 0;
     }
